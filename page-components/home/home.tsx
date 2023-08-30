8e3f9bd813b8ac3e38bb2eb8386499f0d34b6a7d
Cody Marshall <codymstuntsx@gmail.com>
2023-08-30 16:20:53 -0700

    UPDATED: types, useeffect and images

--- page-components/home/home.tsx
+++ page-components/home/home.tsx
@@ -1,26 +1,87 @@
 "use client";
 
-import { useState } from "react";
-import { AOM_COMPETITIONS } from "@/data/aom";
-import YearInput from "./components/yearInput/yearInput";
+import { useEffect, useState } from "react";
+import CompetitionSelect from "./components/competitionSelect/competitionSelect";
 import DisplayBoard from "./components/displayBoard/displayBoard";
+import Image from "next/image";
+import { useCompetitionInfo } from "@/hooks/useCompetitionInfo";
 
-type Event = "Style" | "Speed" | "Skill";
+interface YearEvent {
+  year: number;
+  date: string;
+  location: string;
+  events: {
+    style: {
+      men: {
+        name: string;
+        country: string;
+        place: number;
+      }[];
+      women?: {
+        name: string;
+        country: string;
+        place: number;
+      }[];
+    };
+  };
+}
 
 export default function Home() {
-  const [year, setYear] = useState<number>(2021);
+  const [year, setYear] = useState<number>(2022);
   const [competition, setCompetition] = useState<string>("AOM");
-  const [event, setEvent] = useState<Event>("Style");
-  let shownEvent = AOM_COMPETITIONS.years.find(
-    (eventYear) => eventYear.year === year
-  );
+  const [shownEvent, setShownEvent] = useState<YearEvent | undefined>();
+  const { loading, competitionInfo } = useCompetitionInfo(competition);
+
+  useEffect(() => {
+    if (!competitionInfo) return;
+
+    const firstYear = competitionInfo.years[0]?.year;
+
+    const selectedEvent = competitionInfo.years.find(
+      (event: YearEvent) => event.year === year
+    );
+
+    if (selectedEvent) {
+      setShownEvent(selectedEvent);
+    } else {
+      setYear(firstYear);
+    }
+  }, [year, competitionInfo, shownEvent]);
+
+  if (loading && !competitionInfo) {
+    return <p>Loading</p>;
+  }
 
   return (
-    <main className="flex flex-col justify-between p-24">
-      <h1 className="text-4xl font-bold">Parkour Competitions</h1>
-      <h2 className="text-2xl font-bold">{AOM_COMPETITIONS.name}</h2>
-      <YearInput year={year} setYear={setYear} />
-      {shownEvent && <DisplayBoard shownEvent={shownEvent} event={event} />}
+    <main className="flex flex-col justify-between bg-black h-screen relative">
+      <Image
+        className="absolute top-0 left-0 z-[1] grayscale opacity-50"
+        src={competitionInfo?.background || "/images/background.jpg"}
+        alt="background"
+        style={{ objectFit: "cover" }}
+        width={1920}
+        height={1080}
+        placeholder="blur"
+        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAArElEQVQ4T2NkoBAwUqifw"
+      />
+      <div className="p-24 z-[2]">
+        <h1 className="text-4xl font-bold text-white text-center">
+          Parkour Competitions
+        </h1>
+        <CompetitionSelect
+          year={year}
+          setYear={setYear}
+          competition={competition}
+          setCompetition={setCompetition}
+        />
+        {shownEvent && (
+          <DisplayBoard
+            shownEvent={shownEvent}
+            eventName={competitionInfo?.name}
+            host={competitionInfo?.host}
+          />
+        )}
+      </div>
     </main>
   );
 }
