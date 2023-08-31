dbeae8ed15a88b4c893391413cd34efdd5a5eca6
Cody Marshall <codymstuntsx@gmail.com>
2023-08-30 18:46:15 -0700

    UPDATED: classes to show better on mobile

--- page-components/home/components/displayBoard/components/eventInfo/eventInfo.tsx
+++ page-components/home/components/displayBoard/components/eventInfo/eventInfo.tsx
@@ -12,26 +12,35 @@ export default function EventInfo({
   host,
 }: EventInfoProps) {
   return (
-    <div className="flex flex-col justify-center bg-white rounded-lg p-8 mb-8 lg:w-1/2">
-      <h2 className="text-4xl font-bold text-center text-black">{eventName}</h2>
-      <h4 className="text-2xl font-bold text-center text-gray-600 mb-8">
+    <div className="flex flex-col justify-center bg-white rounded-lg p-4 md:p-8 mb-8 lg:w-1/2">
+      <h2 className="text-2xl md:text-4xl font-bold text-center text-black">
+        {eventName}
+      </h2>
+      <h4 className="text-xl md:text-2xl font-bold text-center text-gray-600 mb-4 md:mb-8">
         {host}
       </h4>
-      <div className="text-center mb-8">
-        <h3 className="text-2xl font-bold text-gray-700">{shownEvent.alias}</h3>
-        <p className="text-gray-500">{shownEvent.year}</p>
+      <div className="text-center mb-4 md:mb-8">
+        <h3 className="text-xl md:text-2xl font-bold text-gray-700">
+          {shownEvent.alias}
+        </h3>
       </div>
-      <div className="flex flex-row justify-between">
-        <div className="flex flex-col">
-          <span className="text-2xl font-bold text-gray-700">Year:</span>
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col mb-4 md:mb-0">
+          <span className="text-xl md:text-2xl font-bold text-gray-700">
+            Year:
+          </span>
           <h3 className="text-gray-500">{shownEvent.year}</h3>
         </div>
-        <div className="flex flex-col">
-          <span className="text-2xl font-bold text-gray-700">Location:</span>
+        <div className="flex flex-col mb-4 md:mb-0">
+          <span className="text-xl md:text-2xl font-bold text-gray-700">
+            Location:
+          </span>
           <h3 className="text-gray-500">{shownEvent.location}</h3>
         </div>
         <div className="flex flex-col">
-          <span className="text-2xl font-bold text-gray-700">Date:</span>
+          <span className="text-xl md:text-2xl font-bold text-gray-700">
+            Date:
+          </span>
           <h3 className="text-gray-500">{shownEvent.date}</h3>
         </div>
       </div>
