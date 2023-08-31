d995aedf6957a18c0561215ef8219f4fd90f064e
Cody Marshall <codymstuntsx@gmail.com>
2023-08-31 13:06:35 -0700

    UPDATED: new props, custom select component

--- page-components/home/components/displayBoard/components/boardToolbar/boardToolbar.tsx
+++ page-components/home/components/displayBoard/components/boardToolbar/boardToolbar.tsx
@@ -1,29 +1,64 @@
+import Select from "@/components/select";
 import React from "react";
 
 export default function BoardToolbar({
+  gender,
+  event,
   setGender,
   setEvent,
+  shownEvents,
+  selectedEventIndex,
+  setSelectedEventIndex,
+  eventDates,
 }: {
+  gender: string;
+  event: string;
   setGender: any;
   setEvent: any;
+  selectedEventIndex: number;
+  setSelectedEventIndex: React.Dispatch<React.SetStateAction<number>>;
+  eventDates: string[];
+  shownEvents: any[];
 }) {
+  function formatDate(date: string) {
+    return new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  }
+
   return (
     <>
-      <select
-        className="text-lg font-bold bg-white rounded p-4 mb-4"
-        onChange={(e) => setGender(e.target.value as any)}
-      >
-        <option value="Men">Men</option>
-        <option value="Women">Women</option>
-      </select>
-      <select
-        className="text-lg font-bold bg-white rounded p-4 mb-4 mx-4"
-        onChange={(e) => setEvent(e.target.value as any)}
-      >
-        <option value="style">Style</option>
-        <option value="speed">Speed</option>
-        <option value="skill">Skill</option>
-      </select>
+      <Select
+        options={["Men, Women"]}
+        value={gender}
+        onChange={(value) => setGender(value)}
+      />
+      <Select
+        options={["Style", "Speed", "Skill"]}
+        value={event}
+        onChange={(value) => setEvent(value)}
+      />
+
+      {shownEvents.length > 1 && (
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Select Event Date
+          </label>
+          <select
+            value={selectedEventIndex}
+            onChange={(e) => setSelectedEventIndex(parseInt(e.target.value))}
+            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          >
+            {eventDates.map((date, index) => (
+              <option key={index} value={index}>
+                {formatDate(date)}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
     </>
   );
 }
